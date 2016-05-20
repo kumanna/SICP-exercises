8a48f039bd77f1d81cd4ade76b3a8f9ce0d29ae5
Kumar Appaiah <a.kumar@alumni.iitm.ac.in>
2016-05-20 10:58:42 +0530

    Add exercise 1.31

--- Chapter_1/ex_1.31.scm
+++ Chapter_1/ex_1.31.scm
@@ -14,6 +14,13 @@
 (display (factorial1 5))
 (newline)
 
+(define (pi-by-4-term k)
+  (if (even? k)
+      (/ (+ k 2) (+ k 1))
+      (/ (+ k 1) (+ k 2))))
+
+(display (* 4 (product1 pi-by-4-term 1 inc 100)))
+(newline)
 (define (product2 term a next b)
   (if (> a b)
       1
@@ -26,4 +33,6 @@
 	(else (product2 identity 1 inc n))))
 
 (display (factorial2 5))
+(newline)
+(display (* 4 (product2 pi-by-4-term 1 inc 100)))
 (newline)

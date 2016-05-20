338b646de535083e0d2d71faaff07fc92ffe9ddc
Kumar Appaiah <a.kumar@alumni.iitm.ac.in>
2016-05-20 10:40:53 +0530

    Add exercise 1.30

---
+++ Chapter_1/ex_1.30.scm
@@ -0,0 +1,12 @@
+(define (sum term a next b)
+  (define (iter a result)
+    (if (> a b)
+	result
+	(iter (next a) (+ result (term a)))))
+  (iter a 0))
+
+(define (identity x) x)
+(define (inc i) (+ i 1))
+(display (sum identity 1 inc 10))
+(newline)
+  
